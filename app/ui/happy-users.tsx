ea7375c818a64b9507746d6456115bdde53d24e3
muhiddin0 <kabraliev2005@gmail.com>
2024-03-11 08:00:18 +0500

    color endiyam change

--- app/ui/happy-users.tsx
+++ app/ui/happy-users.tsx
@@ -38,18 +38,6 @@ const HappyUsers = (props: Props) => {
       </b>
     </section>
   );
-  return (
-    <section className="flex items-center justify-center relative my-16">
-      <Image
-        className="w-full object-cover"
-        src={happyUsers}
-        alt="happy users"
-      />
-      <b className="container absolute text-xl md:text-2xl font-semibold text-center w-full">
-        We&apos;ve helped over 1,000 creatorsre claim control of their audience.
-      </b>
-    </section>
-  );
 };
 
 export default HappyUsers;
